f00f77add9a9c4d50ccf938d199dd09d68dd2879
Matheus Carvalho <matheusdocarvalho@gmail.com>
2025-11-14 18:45:31 -0400

    feat: add footer

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import banner from '../public/banner.png'
 import BookingItem from "./_components/booking-item";
 import { prisma } from "@/lib/prisma";
 import BarbershopItem from "./_components/barbershop-item";
+import Footer from "./_components/footer";
 
 const Home = async () => {
   const recommendedBarbershops = await prisma.barbershop.findMany({
@@ -56,6 +57,7 @@ const Home = async () => {
         ))}
         </div>
       </div>
+      <Footer/>
     </main>
   );
 }
