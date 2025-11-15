a48768078f0fc5ba9f915d8dc7168340d66a1947
Matheus Carvalho <matheusdocarvalho@gmail.com>
2025-11-15 17:37:24 -0400

    feat: creating prompt for IA

--- app/_components/barbershop-item.tsx
+++ app/_components/barbershop-item.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Barbershop } from "../generated/prisma/client";
+import Link from "next/link";
 
 interface BarbershopItemProps {
  barbershop: Barbershop
@@ -8,19 +9,21 @@ interface BarbershopItemProps {
 
 const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
  return ( 
- <div className="relative rounded-xl min-h-[200px] min-w-[290px]">
-  <div className="absolute top-0 left-0 h-full w-full bg-linear-to-t from-black to-transparent rounded-lg  z-10"/>
-  <Image
-   src={barbershop.imageUrl}
-   alt={barbershop.name}
-   fill // Faz com que a imagem ocupe 100% da largura e altura da div pai, que esta com a classe 'relative'
-   className="rounded-xl object-cover"
-   />
-   <div className="absolute bottom-0 left-0 right-0 z-20 p-4">
-    <h3 className="text-background text-lg font-bold">{barbershop.name}</h3>
-    <p className="text-background text-xs">{barbershop.address}</p>
-   </div>
- </div> 
+ <Link href={`/barbershops/${barbershop.id}`}>
+  <div className="relative rounded-xl min-h-[200px] min-w-[290px]">
+    <div className="absolute top-0 left-0 h-full w-full bg-linear-to-t from-black to-transparent rounded-lg  z-10"/>
+    <Image
+    src={barbershop.imageUrl}
+    alt={barbershop.name}
+    fill // Faz com que a imagem ocupe 100% da largura e altura da div pai, que esta com a classe 'relative'
+    className="rounded-xl object-cover"
+    />
+    <div className="absolute bottom-0 left-0 right-0 z-20 p-4">
+      <h3 className="text-background text-lg font-bold">{barbershop.name}</h3>
+      <p className="text-background text-xs">{barbershop.address}</p>
+    </div>
+  </div> 
+ </Link>
  );
 }
  
---
+++ prompt.md
@@ -0,0 +1,35 @@
+<!-- Role Prompt -->
+
+Você é um desenvolvedor full stack sênior, especializado em Next.js
+
+<!-- Contexto -->
+
+Tecnologias utilizadas:
+
+- Next.js
+- Prisma
+- Shadcn/ui
+
+- SEMPRE use shadcn como biblioteca de componentes
+- SEMPRE use componentes que estão em @app_components\ui\page.tsx
+- NUNCA use cores hard-coded do Tailwind, APENAS cores do tema que estão em @app\globals.css
+- Use a página que está em @app\page.tsx como referência para criar e organizar o código.
+- **SEMPRE** use o MCP do Context7 para buscar documentações, sites e APIs.
+
+<!-- Tarefa -->
+
+Crie a página que está em https://www.figma.com/design/GCvGz6IW2ND5G4qU2CaQZI/Aparatus-%7C-Alunos--Copy-?node-id=10-6869&t=0kNWUPttteVIQJyL-4 Usando o figma MCP.
+
+Seja 100% fiel ao Figma **CUSTE O QUE CUSTAR**.
+
+Pegue os dados do banco de dados usando o ID que é recebido como parâmetro na rota.
+
+O botão de "Reservar" NÃO DEVE fazer alta.
+
+O botão de "Copiar" telefone deve copiar o telefone para o clipboard do usuário.
+
+A Imagem do banner da página no topo deve ser a imagem da barbearia no banco de dados.
+
+O botão de voltar no topo da página deve vpçtar a página inicial do projeto.
+
+A imagem de cada serviço deve ser o campo "imageUrl" da tabela "BarbershopService".
