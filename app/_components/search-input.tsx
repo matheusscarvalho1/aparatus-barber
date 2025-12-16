db0193caea01803156ee27014ad614f395fb9d61
Matheus Carvalho <matheusdocarvalho@gmail.com>
2025-12-16 18:01:00 -0400

    feat: rhf on search input

--- app/_components/search-input.tsx
+++ app/_components/search-input.tsx
@@ -2,38 +2,62 @@
 
 import { SearchIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { FormEvent, useState } from "react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form"
+import {z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
 
 const SearchInput = () => {
   const router = useRouter();
-  const [search, setSearch] = useState("");
+  const formSchema = z.object({
+  search: z
+    .string().optional()
+})
 
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-    if (!search.trim()) return;
-    router.push(`/barbershops?search=${encodeURIComponent(search)}`);
-  };
+const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      search: "",
+    },
+  })
+  
 
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    if (!data.search?.trim()) return;
+    router.push(`/barbershops?search=${encodeURIComponent(data.search)}`);
+  }
   return (
-    <form onSubmit={handleSubmit} className="flex items-center gap-2">
-      <Input
-        type="text"
-        placeholder="Pesquise pelos serviços desejados"
-        className="border-border rounded-full"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
-      <Button
-        type="submit"
-        variant="default"
-        size="icon"
-        className="rounded-full"
-      >
-        <SearchIcon />
-      </Button>
-    </form>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <div className="flex items-center gap-2">
+        <FormField
+          control={form.control}
+          name="search"
+          render={({ field }) => (
+            <FormItem className="flex-1">
+              <FormControl>
+                <Input placeholder="Pesquise pelos serviços desejados" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button type="submit" variant="default" size="icon" className="rounded-full" aria-label="Pesquisar">
+          <SearchIcon/>
+        </Button>
+        </div>
+        
+      </form>
+    </Form>
   );
 };
 
